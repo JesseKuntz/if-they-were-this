58cca22312b219c494998f789d7b15f8ac30fb1b
Jesse Kuntz <jkuntz@groupon.com>
2020-10-29 08:19:17 -0500

    feat: test adding smoothscroll polyfill

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import smoothscroll from 'smoothscroll-polyfill'
 import React from 'react'
 
 import Layout from '../components/layout'
@@ -5,16 +6,20 @@ import SEO from '../components/seo'
 
 import { scroll } from '../components/navigate'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="text">
-      <span className="bold-brand">If They Were This</span> is a celebrity quiz
-      game.
-    </div>
-    <div className="text">Put your pop-culture knowledge to the test.</div>
-    <button onClick={scroll}>Start</button>
-  </Layout>
-)
+const IndexPage = () => {
+  smoothscroll.polyfill()
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="text">
+        <span className="bold-brand">If They Were This</span> is a celebrity
+        quiz game.
+      </div>
+      <div className="text">Put your pop-culture knowledge to the test.</div>
+      <button onClick={scroll}>Start</button>
+    </Layout>
+  )
+}
 
 export default IndexPage
