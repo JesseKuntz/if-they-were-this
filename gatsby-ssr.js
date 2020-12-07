9723f11ad5582158cb4941eea27803a2a21c6224
JesseKuntz <jessekuntz96@gmail.com>
2020-12-06 19:36:54 -0600

    fix: add wrapper in ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,23 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'https://graphql.fauna.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        Authorization: `Bearer ${process.env.GATSBY_FAUNA_CLIENT_SECRET}`,
+      },
+    })
+  },
+})
+
+const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+)
+
+export { wrapRootElement }
