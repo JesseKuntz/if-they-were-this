3d63ed587af125e01b4ff2d15fcd10037041892d
JesseKuntz <jessekuntz96@gmail.com>
2021-02-11 11:24:34 -0600

    feat: persist the cache between page loads

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,21 +5,31 @@
  */
 
 import React from 'react';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
 import smoothscroll from 'smoothscroll-polyfill';
 import LazyLoad from 'vanilla-lazyload';
 import 'emoji-sprinkle/window';
 
+const link = new HttpLink({
+  uri: 'https://graphql.fauna.com/graphql',
+  headers: {
+    authorization: `Bearer ${process.env.GATSBY_FAUNA_CLIENT_SECRET}`,
+  },
+});
+
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage),
+});
+
 const client = new ApolloClient({
   uri: 'https://graphql.fauna.com/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
-        Authorization: `Bearer ${process.env.GATSBY_FAUNA_CLIENT_SECRET}`,
-      },
-    });
-  },
+  cache,
+  link,
 });
 
 const wrapRootElement = ({ element }) => (
